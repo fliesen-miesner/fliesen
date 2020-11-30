595bc62a7d5c956a8316f0d6c8aa71af63fd25fd
sss24m <sven.miesner@student.fh-kiel.de>
2020-11-30 16:05:19 +0100

    initial

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$(function(){
+    $("#menu").load("menu.html");
+});
+
+// GOOGLE ANALYTICS
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'UA-177296687-1');
+
