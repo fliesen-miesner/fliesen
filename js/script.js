8d2671e7081fdda017abe92559a67a394f6aee53
sss24m <sven.miesner@student.fh-kiel.de>
2021-06-20 13:34:17 +0200

    removed all google

--- js/script.js
+++ js/script.js
@@ -2,8 +2,3 @@ $(function() {
     $("#menu").load("menu.html");
 });
 
-// GA
-window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
-gtag('js', new Date());
-gtag('config', 'G-MS15QQNZL1');
